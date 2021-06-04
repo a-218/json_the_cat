54df58310085f913af9bca13a45f09fb1ae18c97
Alex Li <lialex0218@gmail.com>
2021-06-04 00:40:37 +0000

    json the cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,37 @@
+
 const request = require('request');
 
-const link = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-const search = process.argv.slice(2);
-const searchLink = link + search;
-
-
-request(searchLink, (error, response, body) => {
-  if (body === '[]') {
-    return console.log('Breed name is not found');
-  }
-  if (error) {
-    return  console.log('request error failed:', error); // Print the error if one occurred
-  }
+const fetchBreedDescription = function (breedName, callback) {
   
-  const data = JSON.parse(body);
-  //console.log(data);
-  console.log(data[0]['description']);
-  //console.log(typeof data);
+  const link = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-  // Print the HTML for the Google homepage.
-});
+  const searchLink = link + breedName;
+
+  request(searchLink, (error, response, body) => {
+    if (error) {
+      callback(error, null); // Print the error if one occurred
+    } else {
+      const data = JSON.parse(body);
+
+      if (data.length === 0) {
+
+        callback('Breed name is not found', null);  //index(error,desc)
+        
+      } else {
+        const descr= data[0]['description']
+        callback(null, descr)
+      }
+    } 
+    // const data = JSON.parse(body);
+    //console.log(data);
+    //console.log(data[0]['description']);
+    //console.log(typeof data);
+    //callback(body);  
+    // Print the HTML for the Google homepage.
+  });
+
+}
+
+
+module.exports = {fetchBreedDescription} ;
